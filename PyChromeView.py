e2005263dcb7e6e28f9fd4251b99f95b21fd05a6
solingho1 <124794068+solingho1@users.noreply.github.com>
2023-04-02 23:21:00 +0300

    Add files via upload

---
+++ PyChromeView.py
@@ -0,0 +1,103 @@
+import os
+import json
+import base64
+import sqlite3
+import win32crypt
+import tabulate
+from Cryptodome.Cipher import AES
+import shutil
+
+
+def getKey() -> bytes:
+    with open(rf"{os.environ['USERPROFILE']}\AppData\Local\Google\Chrome\User Data\Local State") as f:
+        state = json.loads(f.read())
+        key = base64.b64decode(state["os_crypt"]["encrypted_key"])[5:]
+    return win32crypt.CryptUnprotectData(key, None, None, None, 0)[1]
+
+
+def chipGenerate(aesKey: bytes, vec: bytes):
+    return AES.new(aesKey, AES.MODE_GCM, vec)
+
+
+def payloadDecrypt(cipher, payload: bytes) -> bytes:
+    return cipher.decrypt(payload)
+
+
+def passDecrypt(buff: bytes, key: bytes):
+    try:
+        cipher = chipGenerate(key, buff[3:15])
+        decrypted_pass = payloadDecrypt(cipher, buff[15:])
+        return decrypted_pass[:-16].decode()
+
+    except Exception as ex:
+        print(ex)
+        return False
+
+
+def resOut(data):
+    with open('info.txt', 'w') as f:
+        print(tabulate.tabulate(data))
+        f.write(tabulate.tabulate(data))
+
+
+def dbWrapper(func):
+    def wrapper():
+        if func.__name__ == 'getInfo':
+            shutil.copy2(rf"{os.environ['USERPROFILE']}\AppData\Local\Google\Chrome\User Data\default\Login Data",
+                         "differ.db")
+        elif func.__name__ == 'getHistory':
+            shutil.copy2(rf"{os.environ['USERPROFILE']}\AppData\Local\Google\Chrome\User Data\Default\History",
+                         "differ.db")
+        else:
+            shutil.copy2(rf"{os.environ['USERPROFILE']}\AppData\Local\Google\Chrome\User Data\Default\Network\Cookies",
+                         "differ.db")
+
+        try:
+            con = sqlite3.connect("differ.db")
+            cursor = con.cursor()
+            func(cursor)
+            cursor.close()
+            con.close()
+        except Exception as ex:
+            print(ex)
+
+        finally:
+            os.remove("differ.db")
+
+    return wrapper
+
+
+@dbWrapper
+def getInfo(cursor):
+    cursor.execute("SELECT origin_url, username_value, password_value FROM logins")
+    data = [['URL', 'NAME', 'PASSWORD']]
+    for i in cursor.fetchall():
+        url = i[0]
+        username = i[1]
+        decryptedPass = passDecrypt(i[2], getKey())
+        if decryptedPass:
+            data.append([url, username, decryptedPass])
+
+    resOut(data)
+
+
+@dbWrapper
+def getHistory(cursor):
+    cursor.execute("SELECT title, url FROM urls")
+    with open('History.txt', "w") as f:
+        f.write(tabulate.tabulate(cursor.fetchall(), maxcolwidths=[None, 300]))
+
+
+@dbWrapper
+def getCookie(cursor):
+    cursor.execute("SELECT host_key, name, encrypted_value from cookies")
+    data = [['URL', 'NAME', 'COOKIE']]
+    for i in cursor.fetchall():
+        url = i[0]
+        name = i[1]
+        decryptedPass = passDecrypt(i[2], getKey())
+        if decryptedPass:
+            data.append([url, name, decryptedPass])
+    resOut(data)
+
+
